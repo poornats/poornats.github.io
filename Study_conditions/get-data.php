7ec209ac6fdc10de90b30b11f3279d0b24815d4b
poornats <poorna.t.s@gmail.com>
2023-02-15 19:26:59 -0500

    Add files via upload

---
+++ Study_conditions/get-data.php
@@ -0,0 +1,30 @@
+﻿<?php
+ header('Access-Control-Allow-Origin: *');
+ $RID = $_POST['RID'];
+ $opened = 0;
+ $filename = 'logData/logData_all.csv';
+
+ if (($handle = fopen($filename, "r")) !== FALSE) {
+  while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+      
+      
+      if ($data[0] == $RID){
+          $opened = 1;
+      }
+      
+  }
+  
+  fclose($handle);
+}
+
+if ($opened == 0){
+  $f = fopen($filename, 'a');
+    
+  fwrite($f, $RID );
+  
+  fclose($f);
+
+}
+
+echo  json_encode($opened);
+?>
---
+++ Study_conditions/save.php
@@ -0,0 +1,18 @@
+﻿<?php
+    $data = $_POST['logJson'];
+    
+    $obj = json_decode($data);
+    $pid = $obj->{'participantID'};
+    
+    $filename = 'logData/logData'.'_'.strval($pid).'.json';
+    
+    $data = $data. ',
+    ';
+    
+    $f = fopen($filename, 'a');
+    
+    fwrite($f, $data );
+    
+    fclose($f);
+    
+?>
